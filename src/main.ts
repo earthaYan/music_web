c3fb0a806153be2f638543e628dbbc421aece001
yueyueyan <yueyueyan1215@163.com>
2024-04-25 22:06:48 +0800

    delete useless files

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 const app = createApp(App)
-
+const vuetify = createVuetify()
 app.use(createPinia())
 app.use(router)
-
+app.use(vuetify)
 app.mount('#app')
