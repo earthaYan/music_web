ec79a739e14db5ba6af8b2d111ea0a9284c4ba56
yueyueyan <yueyueyan1215@163.com>
2024-05-07 22:32:29 +0800

    clean all code

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_API_SERVER: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <v-app></v-app>
-</template>
-
-<style scoped></style>
