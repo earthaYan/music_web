aa851f45e3bd691c75990cbaa8b73a6b369fd9da
yueyueyan <yueyueyan1215@163.com>
2024-05-14 03:05:20 +0800

    chang layout

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,16 @@
-<script setup lang="ts"></script>
-
 <template>
-  <v-app> <RouterView /> </v-app>
+  <v-app>
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>My Application</v-list-item-title>
+          <v-list-item-subtitle>Vuetify</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+    <v-app-bar>
+      <v-toolbar-title>Application bar</v-toolbar-title>
+    </v-app-bar>
+    <v-main></v-main>
+  </v-app>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
-const vuetify = createVuetify()
+const vuetify = createVuetify({
+  components,
+  directives
+})
 app.use(vuetify)
 app.use(createPinia())
 app.use(router)
